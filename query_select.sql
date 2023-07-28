5b59c265e7a5bf07a95d4b452fce525c4b85ee83
carloeusebi <carloeusebi@gmail.com>
2023-07-28 14:50:37 +0200

    Selected all degrees 'magistrale'

--- query_select.sql
+++ query_select.sql
@@ -11,5 +11,6 @@ SELECT * FROM `courses` WHERE `year` = 1 AND `period` = 'I semestre';
 -- 20/06/2020 (21)
 SELECT * FROM `exams` WHERE `date` = '2020-06-20' AND HOUR(`hour`)>= 14;
 -- 6. Selezionare tutti i corsi di laurea magistrale (38)
+SELECT * FROM `degrees` WHERE `level` = 'magistrale';
 -- 7. Da quanti dipartimenti è composta l'università? (12)
 -- 8. Quanti sono gli insegnanti che non hanno un numero di telefono? (50)

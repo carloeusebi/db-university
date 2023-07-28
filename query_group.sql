4c899b18171935f1c08b2a31111643be9af672cf
carloeusebi <carloeusebi@gmail.com>
2023-07-28 14:56:06 +0200

    Counted yearly enrolled students

--- query_group.sql
+++ query_group.sql
@@ -1,4 +1,10 @@
 -- 1. Contare quanti iscritti ci sono stati ogni anno
+SELECT COUNT(*) as `number_of_enrolled`, YEAR(`enrolment_date`) as `year` FROM `students` GROUP BY `year`;
+
 -- 2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
+
+
 -- 3. Calcolare la media dei voti di ogni appello d'esame
+
+
 -- 4. Contare quanti corsi di laurea ci sono per ogni dipartimento

a4c7695aa2d36d348c4c068207bdfdf4563f1b5d
carloeusebi <carloeusebi@gmail.com>
2023-07-28 14:47:33 +0200

    Selected all courser of first year and first semester

--- query_select.sql
+++ query_select.sql
@@ -6,6 +6,7 @@ SELECT * FROM `courses` WHERE `cfu` >10;
 SELECT * FROM `students` WHERE YEAR(DATEDIFF(NOW(), `date_of_birth`)) > 30;
 -- 4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di
 -- laurea (286)
+SELECT * FROM `courses` WHERE `year` = 1 AND `period` = 'I semestre';
 -- 5. Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del
 -- 20/06/2020 (21)
 -- 6. Selezionare tutti i corsi di laurea magistrale (38)
